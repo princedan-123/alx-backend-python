9c3184fcaf857a6f106453ceadd9cfafc0a45d6d
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-14 18:38:48 +0100

    used typing.Union instead of |

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """A program that uses an annotated function to sum of a list."""
+from typing import Union
 
 
-def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
     """A function that sums up a list of integers and floats.
         ARGS: mxd_lst - the list to be summed
         RETURN: The result of the sum (a float) is returned
