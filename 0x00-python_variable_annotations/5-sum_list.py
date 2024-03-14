bb93a0664d0d1cbfa83e43c008dc8d2215444db0
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-14 19:04:38 +0100

    used type from typing module

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """A programs that defines an annotated function
    which sums up a list of floats.
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """A function that sums up a list of float
         ARGS: input_list - A list of float to be summed up
         RETURN: The result of the sum is returned
