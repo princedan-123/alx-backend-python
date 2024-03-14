f869715be384aabfe5701a833e9fe13c3f3e605e
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-14 19:10:18 +0100

    used Tuple type from typing module

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """A program that creates a tuple using an annotated function."""
 from typing import Union
+from typing import Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
     """Creates a tuple from the parameters.
         ARGS: k - string
               v - integer or float
