f8e6e0ee15930e89a6109e11ac2e09a190d665ab
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-14 10:34:50 +0100

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""A type annotated function that performs addition operation."""
+
+
+def add(a: float, b: float) -> float:
+    """Performs addition operation
+        ARGS a: float
+            b: float
+        RETURN: float
+    """
+    return a + b
