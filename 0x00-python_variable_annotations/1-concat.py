830d7d40af6ed0540af3856016ee71ba626fbb2c
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-14 11:09:10 +0100

    task1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""A program that concatnates a string with an annotated function."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """A function that concatnates two strings.
+       ARGS str1: first string
+            str2: second string
+       RETURN: A concatnated string is returned
+    """
+    return str1 + str2
