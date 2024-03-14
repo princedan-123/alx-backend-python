8b762d361d2e351449eab973fe648589e23536f5
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-14 19:06:48 +0100

    used type List from typing module

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """A program that uses an annotated function to sum of a list."""
 from typing import Union
+from typing import List
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """A function that sums up a list of integers and floats.
         ARGS: mxd_lst - the list to be summed
         RETURN: The result of the sum (a float) is returned
