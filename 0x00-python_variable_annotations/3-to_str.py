b567a6808b44cba1c4ac384d6314dd8413afbc39
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-14 11:27:11 +0100

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A program that returns the string representation of a float data type."""
+
+
+def to_str(n: float) -> str:
+    """A function that takes a float and returns its string representation
+        ARGS n: A float
+        RETURN: A string is returned
+    """
+    return str(n)
