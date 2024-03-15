bb1043f412a42f7895ac1e8129499f84f5e27bcd
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-15 22:41:50 +0100

    documented function

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,4 +7,5 @@ from typing import List
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """A function that takes a interable and returns a list of tuples."""
     return [(i, len(i)) for i in lst]
