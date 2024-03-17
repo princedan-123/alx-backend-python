d4c95c24d23c96630c9204c73a31fec39394e0cd
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-17 17:01:36 +0100

    advance task

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 """A program that will be validated for type annotation using mypy."""
-from typing import Sequence
-from typing import Union
+from typing import List
+from typing import Tuple
 
 
 def zoom_array(
-        lst: Sequence[Union[int, float]], factor: int = 2
-        ) -> Sequence[Union[int, float]]:
+        lst: Tuple, factor: int = 2
+        ) -> List[int]:
     """A function that takes a list and integer and returns a refined list."""
-    zoomed_in: Sequence[Union[int, float]] = [
+    zoomed_in: List[int] = [
             item for item in lst
             for i in range(factor)
             ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
