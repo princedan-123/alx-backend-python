b2898eae65ac826f8ec9dfda8759a8c0b29d3a1b
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-18 18:00:16 +0100

    task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,6 +5,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Any:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """A function that creates a Task class for wait_random coroutine."""
     return asyncio.create_task(wait_random(max_delay))
