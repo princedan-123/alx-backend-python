5e795d4717e6aadb8c43f740805510f0b8a61408
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-18 15:54:49 +0100

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""An asychronous coroutine that generates a list of numbers."""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 0) -> List[float]:
+    """An asycn function that implements another async function
+        Args: n (int) a number that indicates the number of times
+              to implement wait_random function
+              max_delay (int) the duration for the wait_random funcion
+              to sleep.
+        Return: a list is returned
+    """
+    calls = [wait_random(max_delay) for i in range(0, n)]
+    result = await asyncio.gather(*calls)
+    result.sort()
+    return result
