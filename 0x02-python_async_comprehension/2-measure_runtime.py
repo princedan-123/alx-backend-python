2a6a062365e103950245b03b0271191ab8e4af1a
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-19 15:29:45 +0100

    task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Measures the time of execution of four asynchronous tasks."""
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    A function that measures the execution time of four
+    asynchronous tasks.
+    Args: no argument
+    Return: the time in float
+    """
+    start_time = time.perf_counter()
+    await asyncio.gather(
+            async_comprehension(), async_comprehension(),
+            async_comprehension(), async_comprehension()
+            )
+    end_time = time.perf_counter() - start_time
+    return end_time
