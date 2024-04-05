7275b006d9e3a450336060bffed3af796e4fc9d0
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-05 17:27:14 +0100

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,11 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, map, key, expected):
         """A test case for acess_nested_map function."""
         self.assertEqual(access_nested_map(map, key), expected)
+
+    @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
+    def test_access_nested_map_exception(self, map, key):
+        """
+        A test case that test if the acess_nested_map raises an exception.
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(map, key)
