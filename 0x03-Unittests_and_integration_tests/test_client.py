047975ab785112468fc009bf8610c2969d4bf1f7
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-08 10:30:25 +0100

    task 4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,19 @@
+from parameterized import parameterized
+from client import GithubOrgClient
+import unittest
+from unittest.mock import patch
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """A class that inherits TestCase."""
+    @parameterized.expand(['google', 'abc'])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_json_object):
+        """
+            A test case that test GithubOrgClient.org
+            returns the correct value.
+        """
+        client = GithubOrgClient(org_name)
+        client.org()
+        full_url = client.ORG_URL.format(org=client._org_name)
+        mock_json_object.assert_called_once_with(full_url)
