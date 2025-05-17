c05306ddfca1587211f1c05bf6281be7c9a87484
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-05-17 20:24:39 +0100

    test file for testing  4-stream functions

---
+++ python-generators-0x00/4-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A script that tests the functionality of the stream_user_ages
+   function and average age function.
+"""
+average_age = __import__('4-stream_ages').average_age
+stream_user_ages = __import__('4-stream_ages').stream_user_ages
+average_age()
