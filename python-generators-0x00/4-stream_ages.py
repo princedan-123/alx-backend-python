6ac8b02ff0ae603af286a0a3c0d61213eb6c8f9c
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-05-17 20:23:57 +0100

    calculated average age without using mysql avg function

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,49 @@
+"""A script that generate users average age
+without using mysql avg function.
+"""
+from dotenv import load_dotenv
+import os
+from mysql.connector import connect
+
+load_dotenv()
+user = os.getenv('user')
+password = os.getenv('password')
+database = os.getenv('database')
+
+def stream_user_ages():
+    """A function that streams users age."""
+    try:
+        db = connect(
+            host='localhost',
+            user=user,
+            password=password,
+            database=database,
+        )
+        cursor = db.cursor(dictionary=True)
+        cursor.execute(
+            """
+            SELECT age FROM user_data; 
+            """
+        )
+        result = cursor.fetchall()
+        for age in result:
+            yield age
+    except Exception as error:
+        print(
+            f'An error occured while trying to retrieve age from database: {error}'
+            )
+    finally:
+        cursor.close()
+        db.close()
+        
+
+def average_age():
+    """A function that calculates the average age of users without
+       using the mysql avg function.
+    """
+    age = 0
+    count = 0
+    for row in stream_user_ages():
+        age += row['age']
+        count += 1
+    print(f'Average age of users: {age /count}')
