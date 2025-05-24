6ac1da02886468d236f908e9fbf4f4682733ecf8
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-05-24 17:11:28 +0100

    used functools to wrap the function

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,30 @@
+"""A script that implemets a decorator that logs a query before executing it."""
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+""" YOUR CODE GOES HERE"""
+def log_queries(func):
+    """ A decorator function that extends
+        a function by logging its query.
+    """
+    @functools.wraps(func)
+    def wrapper(query):
+        print(query)
+        return func(query)
+    return wrapper
+        
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
+print(users)
