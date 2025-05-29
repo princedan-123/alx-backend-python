34049e73da145585d23aa0c1e42476e6723198a4
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-05-29 20:01:25 +0100

    alx unittesting tutorial

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,105 @@
+import unittest
+from parameterized import parameterized
+from unittest.mock import patch, PropertyMock
+from client import GithubOrgClient
+
+class TestGithubOrgClient(unittest.TestCase):
+    """A class that test the client.GithubOrgClient class."""
+    @parameterized.expand([
+        ('google_test', 'google', {"login": "google","id": 1342004}),
+        ('abc_test', 'abc', {"message": "Not Found", "status": "404"})
+    ])
+    @patch('client.get_json')
+    def test_org(self, name, org, expected, mock_get_json):
+        """A test case to test the org method of GithubOrgClient class."""
+
+        mock_get_json.return_value = expected
+        git_client = GithubOrgClient(org)
+        output = git_client.org
+        mock_get_json.assert_called_once_with('https://api.github.com/orgs/{org}'.format(org=org))
+        self.assertEqual(output, expected)
+    
+    def test_public_repos(self):
+        """A test case to test GithubOrgClient.public_repos."""
+    
+    @parameterized.expand([
+            (
+                'test_google',
+                'google',
+                'https://api.github.com/orgs/google/repos'
+            ),
+            (
+                'test_abc',
+                'abc',
+                'KeyError: repos_url'
+            )
+
+        ]
+    )
+    def test_public_repos_url(self, name, org_name, expected):
+        """
+        A test case to test GithubOrgClient._public_repos_url.
+        GithubOrgClient._public_repos_url depends on GithubOrgClient.org
+        therefore GithubOrgClient.org will be mocked using patch
+        """
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+            google_payload = {
+                "login": "google",
+                    "id": 1342004,
+                    "node_id": "MDEyOk9yZ2FuaXphdGlvbjEzNDIwMDQ=",
+                    "url": "https://api.github.com/orgs/google",
+                    "repos_url": "https://api.github.com/orgs/google/repos","events_url": "https://api.github.com/orgs/google/events","hooks_url": "https://api.github.com/orgs/google/hooks","issues_url": "https://api.github.com/orgs/google/issues","members_url": "https://api.github.com/orgs/google/members{/member}"
+            }
+            bad_payload = {
+                "message": "Not Found",
+                    "documentation_url": "https://docs.github.com/rest/orgs/orgs#get-an-organization",
+                    "status": "404"   
+            }
+            client = GithubOrgClient(org_name)
+            if org_name == 'google':
+                mock_org.return_value = google_payload
+                result = client._public_repos_url
+                self.assertEqual(result, expected)
+            if org_name == 'abc':
+                mock_org.return_value = bad_payload
+                with self.assertRaises(KeyError):
+                    client._public_repos_url
+
+class TestGithubOrgClient(unittest.TestCase):
+    """A test class to unit-test GithubOrgClient.public_repos."""
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """A test case to test GithubOrgClient.public_repos."""
+        payload = [
+            {
+                "name": "google-repo-one",
+                "license": {
+                    "key": "apache-2.0"
+                    }
+            },
+            {
+                "name": "google-repo-two",
+                "license": {
+                    "key": "mit"
+                    }
+            },
+            {
+                "name": "google-repo-three",
+                "license": None
+            },
+            {
+                "name": "google-repo-four"
+            }
+        ]
+        mock_get_json.return_value = payload
+        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = 'https://api.github.com/orgs/google/repos'
+            client = GithubOrgClient('google')
+            public_repo = client.public_repos()
+            expected_public_repo = [
+                "google-repo-one", "google-repo-two",
+                "google-repo-three", "google-repo-four"
+                ]
+            self.assertEqual(public_repo, expected_public_repo)
+            mock_get_json.assert_called_once()
+            mock_public_repos_url.assert_called_once()
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,58 @@
+"""A test module to test the utils.access_nested_map function."""
+from parameterized import parameterized
+import unittest
+from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import get_json
+
+class TestAccessNestedMap(unittest.TestCase):
+    """A test class to test access_nested_map function."""
+    @parameterized.expand(
+        [
+            ('single_dictionary', {"a": 1}, ("a",), 1),
+            ('double_nested_dictionary', {"a": {"b": 2}}, ("a",), {"b": 2}),
+            ('two_keys', {"a": {"b": 2}}, ("a", "b"), 2)
+        ]
+    )
+    def test_access_nested_map(self, name, map, path, expected):
+        """
+            A test method to test the access_nested_map using parameterized inputs.
+            Args: name (str) - test label
+                  map(dict) - dictionary to be tested
+                  path(sequence) - the keys of the dictionary.
+        """
+        self.assertEqual(access_nested_map(map, path), expected)
+    
+    @parameterized.expand([
+        ('no dictionary', {}, ("a",), KeyError),
+        ('excess keys', {"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, name, map, path, expected):
+        """A test case to check for exceptions."""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(map, path)
+            self.assertEqual(path, str(context.exception))
+
+class TestGetJson(unittest.TestCase):
+    """This class uses mocks a function."""
+    @parameterized.expand([
+        ('True', 'http://example.com', {'payload': True}),
+        ('False', 'http://holberton.io', {'payload': False})
+    ])
+    def test_get_json(self, name, url, expected):
+        """Test that utils.get_json returns the expected result."""
+        with patch('utils.requests.get') as mock_get:
+            def mock_behaviour(url):
+                response = Mock()
+                if url == 'http://example.com':
+                    response.json.return_value = {"payload": True}
+                    return response
+                if url == 'http://holberton.io':
+                    response.json.return_value = {"payload": False}
+                    return response
+                response.json.return_value = ValueError('error no value')
+                return response
+            mock_get.side_effect = mock_behaviour
+            output = get_json(url)
+            self.assertEqual(output, expected)
+            mock_get.assert_called_once_with(url)
