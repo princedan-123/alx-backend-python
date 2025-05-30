9c1a57d53721ffbc58fc7bbe84eeca8b436814af
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-05-30 16:26:39 +0100

    made test pep8 compliant

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,11 +4,12 @@ from parameterized import parameterized
 from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 
+
 class TestGithubOrgClient(unittest.TestCase):
     """A class that test the client.GithubOrgClient class."""
     @parameterized.expand(
         [
-            ('google', {"login": "google","id": 1342004}),
+            ('google', {"login": "google", "id": 1342004}),
             ('abc', {"message": "Not Found", "status": "404"})
         ]
     )
@@ -17,9 +18,11 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.return_value = expected
         git_client = GithubOrgClient(org)
         output = git_client.org
-        mock_get_json.assert_called_once_with('https://api.github.com/orgs/{org}'.format(org=org))
+        mock_get_json.assert_called_once_with(
+            'https://api.github.com/orgs/{org}'.format(org=org)
+            )
         self.assertEqual(output, expected)
-    
+
     @parameterized.expand([
             (
                 'google',
@@ -29,23 +32,29 @@ class TestGithubOrgClient(unittest.TestCase):
                 'abc',
                 'KeyError: repos_url'
             )
-
         ]
     )
-
     def test_public_repos_url(self, org_name: str, expected: str) -> None:
-        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+        with patch(
+            'client.GithubOrgClient.org', new_callable=PropertyMock
+        )as mock_org:
             google_payload = {
                 "login": "google",
-                    "id": 1342004,
-                    "node_id": "MDEyOk9yZ2FuaXphdGlvbjEzNDIwMDQ=",
-                    "url": "https://api.github.com/orgs/google",
-                    "repos_url": "https://api.github.com/orgs/google/repos","events_url": "https://api.github.com/orgs/google/events","hooks_url": "https://api.github.com/orgs/google/hooks","issues_url": "https://api.github.com/orgs/google/issues","members_url": "https://api.github.com/orgs/google/members{/member}"
+                "id": 1342004,
+                "node_id": "MDEyOk9yZ2FuaXphdGlvbjEzNDIwMDQ=",
+                "url": "https://api.github.com/orgs/google",
+                "repos_url": "https://api.github.com/orgs/google/repos",
+                "events_url": "https://api.github.com/orgs/google/events",
+                "hooks_url": "https://api.github.com/orgs/google/hooks",
+                "issues_url": "https://api.github.com/orgs/google/issues",
+                "members_url":
+                "https://api.github.com/orgs/google/members{/member}"
             }
             bad_payload = {
                 "message": "Not Found",
-                    "documentation_url": "https://docs.github.com/rest/orgs/orgs#get-an-organization",
-                    "status": "404"   
+                "documentation_url":
+                "https://docs.github.com/rest/orgs/orgs#get-an-organization",
+                "status": "404"
             }
             client = GithubOrgClient(org_name)
             if org_name == 'google':
@@ -59,6 +68,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @patch('client.get_json')
     def test_public_repos(self, mock_get_json):
+        """Tests the test_public_repos."""
         payload = [
             {
                 "name": "google-repo-one",
@@ -81,8 +91,13 @@ class TestGithubOrgClient(unittest.TestCase):
             }
         ]
         mock_get_json.return_value = payload
-        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
-            mock_public_repos_url.return_value = 'https://api.github.com/orgs/google/repos'
+        with patch(
+            'client.GithubOrgClient._public_repos_url',
+            new_callable=PropertyMock
+        ) as mock_public_repos_url:
+            mock_public_repos_url.return_value = """
+            https://api.github.com/orgs/google/repos
+            """
             client = GithubOrgClient('google')
             public_repo = client.public_repos()
             expected_public_repo = [
