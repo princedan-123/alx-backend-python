0d55b236047776506efee6d3b6d0cbde11bc7206
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-06-05 14:00:34 +0100

    added field validation

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,6 +3,18 @@ from rest_framework import serializers
 from .models import User, Conversation, Message
 
 class SerializeUser(serializers.ModelSerializer):
+    phone_number_present = serializers.SerializerMethodField()
+    def get_phone_number_present(self, obj):
+        if obj.phone_number is not None:
+            return True
+    
+    def validate_first_name(self, value):
+        if ';' in value:
+            raise serializers.ValidationError('name contains ; which makes it vulnerable to injection')
+    
+    def validate_last_name(self, value):
+        if ';' in value:
+            raise serializers.ValidationError('name contains ; which makes it vulnerable to injection')
     class Meta:
         model = User
         fields = '__all__'
