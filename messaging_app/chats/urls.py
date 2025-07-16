25c1fb1f6c7caaa6ae53ecb783d6273ea690e92f
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-07-16 11:42:02 +0100

    modified urls file to meet checker requirement

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import router
 from .views import UserViewset, MessageViewset, ConversationViewset
 
-router = DefaultRouter()
+router = router.DefaultRouter()
 router.register(r'user', UserViewset)
 router.register(r'message', MessageViewset)
 router.register(r'Conversation', ConversationViewset)
