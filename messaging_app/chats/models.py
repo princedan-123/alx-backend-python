9a15f5b02d24a16b9a0bafd4166bfd52423a4af9
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-07-16 06:41:32 +0100

    created serializer classes and modified models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
     phone_number = models.CharField(max_length=21, null=False, blank=False, unique=True)
+    country_code = models.CharField(max_length=4, null=False, blank=False)
     password = models.CharField(max_length=128, null=False, blank=False)
     user_id = models.UUIDField(default=uuid4, primary_key=True)
     def __str__(self):
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -131,3 +131,4 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+AUTH_USER_MODEL = 'chats.User'
