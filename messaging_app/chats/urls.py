a98deb631c0e862ae5be60a68c5a58e6ef4114c0
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-07-17 09:28:15 +0100

    changed Viewset to ViewSet

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework_nested.routers import NestedDefaultRouter
-from .views import UserViewset, MessageViewset, ConversationViewset
+from .views import UserViewSet, MessageViewSet, ConversationViewSet
 
 route = routers.DefaultRouter()
-route.register(r'user', UserViewset)
-route.register(r'message', MessageViewset)
-route.register(r'Conversation', ConversationViewset)
+route.register(r'user', UserViewSet)
+route.register(r'message', MessageViewSet)
+route.register(r'Conversation', ConversationViewSet)
 conversation_nested_router = NestedDefaultRouter(route, r'conversation', lookup='conversation')
-conversation_nested_router.register(r'messages', MessageViewset, basename='conversation-message')
+conversation_nested_router.register(r'messages', MessageViewSet, basename='conversation-message')
 urlpatterns = [
     path('', include(route.urls)),
     path('', include(conversation_nested_router.urls))
