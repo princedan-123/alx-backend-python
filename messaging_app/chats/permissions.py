1477e7f3d18364dade8cb952776c0cad85a06776
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-07-22 05:45:51 +0100

    changed import path to meet checkers requirement

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,6 +1,6 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsOwner(BasePermission):
+class IsOwner(permissions.BasePermission):
     """A permission class that ensures a user can only access his message."""
     def has_object_permission(self, request, view, obj):
         if request.user == obj.user:
