bacfd855bca7a0eb0e6168ad6932c84561a83456
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-07-23 05:49:06 +0100

    implemeted time restrictive middleware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,7 @@
 from .models import User
-from datetime import datetime
+from datetime import time, datetime
+from django.utils import timezone
+from django.http import HttpResponseForbidden
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -11,4 +13,33 @@ class RequestLoggingMiddleware:
         print(f'{datetime.now()} - User: {user} - Path: {request.path}')
         with open('requests.log', 'w', encoding='utf-8') as file:
             file.write(f'{datetime.now()} - User: {user} - Path: {request.path}')
+        return self.get_response(request)
+
+class RestrictAccessByTimeMiddleware:
+    """
+        A middleware that restricts access to the app during certain hours
+        of the day.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        current_server_time_date = timezone.now()
+        current_server_date = current_server_time_date.date()
+        nine_pm = datetime.combine(current_server_date, time(21, 0,0))
+        six_pm - datetime.combine(current_server_date, time(18,00))
+        nine_pm_aware = timezone.make_aware(
+            nine_pm, timezone.get_current_timezone()
+            )
+        six_pm_aware = timezone.make_aware(
+            six_pm, timezone.get_current_timezone()
+            )
+        if current_server_time_date > nine_pm_aware:
+            return HttpResponseForbidden(
+                '403 forbidden you can only used this app between 6-9pm'
+                )
+        if current_server_time_date < six_pm_aware:
+            return HttpResponseForbidden(
+                '403 forbidden you can only used this app between 6-9pm'
+                )
         return self.get_response(request)
