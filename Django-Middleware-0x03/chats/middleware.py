44d4ae5e167e0ed33e2b63fe58e86362d3ebf132
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-07-24 10:42:26 +0100

    changed file path

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -12,7 +12,7 @@ class RequestLoggingMiddleware:
         """Implements a logging feature that logs data to a file."""
         user = request.user
         print(f'{datetime.now()} - User: {user} - Path: {request.path}')
-        with open('./requests.log', 'w', encoding='utf-8') as file:
+        with open('requests.log', 'w', encoding='utf-8') as file:
             file.write(f'{datetime.now()} - User: {user} - Path: {request.path}')
         return self.get_response(request)
 
