91785f9e124a6d449685547bf48d61377fb9dc9f
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-07-25 09:48:37 +0100

    add get_paginated_response method to customize paginated response

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,8 +1,25 @@
 """Custom paginations are defined here."""
 from rest_framework.paginations import PageNumberPagination
+from rest_framework.response import Response
 
 class CustomizedPageNumberPagination(PageNumberPagination):
     page_size = 20
     page_query_param = 'page'
     page_size_query_param = 'page_size'
-    max_page_size = 50
+    max_page_size = 50,
+
+    def get_paginated_response(self, data):
+        """Customizes paginated json response output."""
+        pagination_meta_data = {
+            'current_page_number' : self.page.number,
+            'paginator_count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link()
+        }
+        return Response(
+            {
+                'meta': pagination_meta_data,
+                'data': data
+            }
+        )
