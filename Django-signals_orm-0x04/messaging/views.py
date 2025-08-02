271b1023cc38f826ca201e08e7bb846cad38b828
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-08-02 13:59:00 +0100

    added another view

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -26,3 +26,44 @@ class delete_user(viewsets.ViewSet):
                 {'An error occured': str(e)},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
                 )
+
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Message, Conversation
+from .serializers import MessageSerializer, ConversationSerializer, UserSerializer
+from rest_framework.decorators import action
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+
+User = get_user_model()
+
+class ConversationView(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    @action(methods=['get'], detail=True)
+    @method_decorator(cache_page(60))
+    def my_messages(self, request, pk=None):
+        """list all messages within a conversation."""
+        try:
+            conversation = Conversation.objects.get(pk=pk)
+        except Conversation.DoesNotExist:
+            return Response(
+                {'Error': f'this conversation {pk} does not exist'},
+                status=404
+                )
+        messages = conversation.messages.all()
+        serialized_message = MessageSerializer(messages, many=True)
+        return Response(
+            {f'messages_in_{pk}_conversation':serialized_message.data}
+            )
+@method_decorator(cache_page(60))
+class MessageView(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+@method_decorator(cache_page(60))
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
