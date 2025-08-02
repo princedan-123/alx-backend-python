29804406846e9dff32974dbf31cdf66a75e5a15e
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-08-02 17:15:18 +0100

    add custom manager

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,17 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# creating a custom manager for custom logic
+class UnreadMessagesManager(models.Manager):
+    def get_queryset(self):
+        """Returns only unread messages."""
+        return super().get_queryset().filter(is_read=False)
+
+
 #  used django default User instance
+
+class Conversation(models.Model):
+    participants = models.ManyToManyField(User, blank=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 
 class Message(models.Model):
     content = models.TextField()
+    is_read = models.BooleanField(default=False)
+    parent_message field = models.ForeignKey(
+        'Message', on_delete=models.CASCADE, null=True,
+        blank=True, related_name='replies'
+        )
     edited = models.BooleanField(default=False)
     edited_at = models.DateTimeField(null=True, blank=True)
     edited_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='edited_messages', null=True, blank=True)
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     timestamp = models.DateTimeField(auto_now_add=True)
-    
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, null=False, blank=False,
+        related_name='messages'
+        )
+    objects = models.Manager()
+    UnreadMessagesManager = UnreadMessagesManager()
+
     def __str__(self):
         return f'{self.sender} to {self.receiver}'
 class MessageHistory(models.Model):
